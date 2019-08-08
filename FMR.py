9f9f3d922f6c966e58cc1f72385121418de87beb
Purnima Balakrishnan <purnimy.b@gmail.com>
2019-08-08 15:20:36 -0700

    Revert "Merge branch 'statsproject' into master"
    This reverts commit a298a0df8f5585624d702be1fd2e6653d7015e3c, reversing
    changes made to ee6ed314ebeb21c484a7e1e53e475997173647ac.

--- FMR.py
+++ FMR.py
@@ -5,8 +5,8 @@ from scipy.optimize import curve_fit
 #TODO: figure out how to do background subtraction not just with H>0
 
 def readFile(filename):
-	#H, dPdH, phase
-    return np.loadtxt(filename, unpack=True, delimiter=',') 
+    H, dPdH, phase = np.loadtxt(filename, unpack=True, delimiter=',')
+    return H, dPdH, phase
 
 def readFileTruncatePos(filename):
     H, dPdH, phase = np.loadtxt(filename, unpack=True, delimiter=',')
@@ -16,16 +16,6 @@ def readFileTruncatePos(filename):
         dPdH = dPdH[:zero[0]]
         phase = phase[:zero[0]]
     return H, dPdH, phase
-
-def unwrapPhase(phase):
-	for i in np.arange(len(phase)-1):
-		phiplus1 = phase[i+1]
-		phi = phase[i]
-		diffs = np.array([360., 0., -360.])
-		whichloop = np.argmin(np.abs(phiplus1-phi+diffs)) #compare difference of angles
-		if whichloop != 1:
-			phase[i+1] += diffs[whichloop]
-	return phase
 
 def LorentzianDerivative(x, a, x0, theta, b):
     denom = (b**2 + (x-x0)**2)**2
@@ -93,19 +83,6 @@ def guessNplus1(oldguess,width=1,pos='Right'): #TODO: WRITE THIS
 def linearBG(x, m, b):
     return m*x+b
     
-#def subtractBGIterative(H, dPdH, BGfunc=linearBG):
-	#1. find background region with hypothesis test, and use this to measure standard deviation / error of points
-#	if np.sign(H[0]) == np.sign(H[-1]):
-#		steppoints = 20
-#		leftregion = 20
-#		stillstraight = True
-#		fit = np.array([0, 0])
-#		while stillstraight:
-#			fit = curve_fit(BGfunc, H[:leftregion], dPdH[:leftregion], fit)
-#			chi2 = np.sum((dPdH[:leftregion] - linearBG(H[:leftregion]))**2)
-#	else:
-#		zero = np.argmin(np.abs(H))
-
 def subtractBG(H, dPdH, window=6, BGfunc=linearBG):
     #need to fix this to work with negative H
     guessmag, guessres, guesswidth = guesses(H,dPdH)
@@ -131,23 +108,15 @@ def subtractBG(H, dPdH, window=6, BGfunc=linearBG):
     if len(bg) > 1:
         guess = np.array([0, 0])
         fit = curve_fit(BGfunc, bgH, bg, guess)
-
-        #extract noise
-        fitBGregion = BGfunc(np.array(bgH), *(fit[0]))
-        s2 = np.sum((bg - fitBGregion)**2) / (len(bgH)-2) #-2 b/c 2 points needed to make a line
-        errordPdH = np.ones_like(bgH)*np.sqrt(s2)
-        
-        #because all of the errors are equal, the covariance matrix of the fit is independent of the errors
-        
-        fitBG = BGfunc(H, *(fit[0]))
+        fitBG = linearBG(H, *(fit[0]))
         plt.plot(H,dPdH,'.')
         plt.plot(H,fitBG)
         plt.vlines(H[bgend],np.min(dPdH),np.max(dPdH),linestyles='dotted')
         plt.vlines(H[bgstart],np.min(dPdH),np.max(dPdH),linestyles='dotted')
     else:
-        fit = (np.array([0.,0.]), np.array([[0.,0.],[0.,0.]]))
+        fitBG = np.zeros_like(H)
     
-    return fit, s2
+    return fitBG
 
 def fitFMR(H, dPdHoffset, guess, debug=False, guessplt=1):
     fit = curve_fit(lambda x,*guess: LorentzianDerivativeNWrapper(x,guess), H, dPdHoffset, guess)
